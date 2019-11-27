dee0b99071ec976e290fa604008707551f1ea4da
adamw <adamwojtczak807@gmail.com>
2019-11-27 23:57:04 +0100

    działające bartkowo

--- src/SynchronizedReflectionTest.java
+++ src/SynchronizedReflectionTest.java
@@ -1,43 +1,74 @@
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class SynchronizedReflectionTest {
-    public static void main(String[] args) {
-        SynchronizedReflectionTest test = new SynchronizedReflectionTest();
-        String[] strings = {"aaaa", "bb", "ccccccccccccc", "dddddd"};
-        for(String s : strings)
-        {
+        private static String[] strings = {"aaaaaaaaaaa", "bbbbb", "cc", "dddddddddddddddddddddddddddd"};
 
-                new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        for( char c : s.toCharArray())
-                        {
-                        try {
-                            Method someThing = SynchronizedReflectionTest.class.getDeclaredMethod("someThing", new Class[]{char.class});
-                            someThing.invoke(test, c);
-                        } catch (NoSuchMethodException e) {
-                            e.printStackTrace();
-                        } catch (InvocationTargetException e) {
-                            e.printStackTrace();
-                        } catch (IllegalAccessException e) {
-                            e.printStackTrace();
+        static List<WritingThread> queue = new CopyOnWriteArrayList<>();
+        static Notificator notificator = new Notificator();
+        public static void main(String[] args) {
+
+            for (String s : strings) {
+                new WritingThread(s,notificator).start();
+            }
+//            try {
+//                Thread.sleep(100);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+            while (queue.size() != 0) {
+                for (WritingThread thread : queue) {
+                    synchronized (thread) {
+                        if (thread.getState().equals(Thread.State.WAITING)) {
+                            synchronized (thread.notificator) {
+                                thread.notificator.notify();
+                                try {
+                                    notificator.wait();
+                                } catch (InterruptedException e) {
+                                    e.printStackTrace();
+                                }
+                            }
                         }
+                        else break;
+                    }
+                }
+            }
+
+        }
+        private static class Notificator {
+            public Notificator() { }
+        }
+        private static class WritingThread extends Thread {
+
+            String tobewritten;
+            protected Notificator notificator;
+
+            public WritingThread(String tobewritten , Notificator notificator) {
+                this.tobewritten = tobewritten;
+                this.notificator = notificator;
+            }
+
+            @Override
+            public void run() {
+                queue.add(this);
+                for (char c : tobewritten.toCharArray()) {
+                    synchronized (notificator) {
+                        try {
+                            notificator.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
                         }
                     }
-                }).start();
+                    System.out.print(c);
+                    synchronized (notificator){
+                        notificator.notify();
+                    }
+                }
+                queue.remove(this);
 
-
+            }
         }
-    }
 
-    public void someThing(char c)
-    {
-        System.out.print(c);
-        try {
-        Thread.sleep(200);
-        } catch (InterruptedException e) {
-        e.printStackTrace();
-        }
-    }
 }
